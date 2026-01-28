39ab22101259281835d309b971591c6eea767292
Himanshu Sharma <himanshuitm27@gmail.com>
2026-01-28 16:38:29 +0530

    Create basics1.java

---
+++ basics1.java
@@ -0,0 +1,18 @@
+public class Himanshu{
+
+    public static void main(String[] args) {
+        
+    //     System.out.println("Kanye West -- RunAway");
+    //     System.out.println("kid Cudi --- Moui waowi");
+    //     System.out.println("MGMT - when you die ");
+    // 
+            int  b = 1 ;            
+
+            System.out.println("Range of the Integer is ");
+            System.out.println("The value of integer is " + b);
+            System.out.println("The maximum range of the integer is "+Integer.MAX_VALUE);
+            System.out.println("the minimum range of the integer is " + Integer.MIN_VALUE);
+
+
+    }
+}
