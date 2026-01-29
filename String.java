95379d6ca4f138a3d90d09a4eed20b4b6590f53f
Himanshu Sharma <himanshuitm27@gmail.com>
2026-01-29 12:51:24 +0530

    Create String.java

---
+++ String.java
@@ -0,0 +1,8 @@
+
+            String s1 = "Kanye";
+            String s2 = "Kanye";
+            String s3 = new String("Kanye");
+
+            System.out.println(s1==s2);
+            System.out.println(s1==s3);
+            System.out.println(s1.equals(s3));
